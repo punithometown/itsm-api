f7bcf6ea689801d761b9c6bb696fb3b496e8ff4e
punit pratap singh <punitpratapsingh@Aashi-MacBook.local>
2026-04-16 11:21:46 +0530

    check

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:8080', // Your React frontend URLs
+    origin: 'http://localhost:8080', // Your React frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
